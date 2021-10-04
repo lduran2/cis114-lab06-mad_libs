fa521dc5b36dfa8b21cd9bfcec3e47a571715992
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2021-10-04 17:20:32 -0400

    ask for a phrase of each type for that mad lib

--- scripts/create_mad_libs.js
+++ scripts/create_mad_libs.js
@@ -1,3 +1,4 @@
+/* all of the mad libs */
 const MAD_LIBS = [
 	{
 		'template': function (input) { return `${input[0]}, ${input[1]}, ${input[2]}, ${input[3]}, ${input[4]}, ${input[5]}`; },
@@ -9,4 +10,15 @@ const MAD_LIBS = [
 const I_MAD_LIBS = Math.floor(Math.random() * MAD_LIBS.length);	/* generate random number [0, MADLIBS.length[ */
 const RANDOM_MAD_LIB = MAD_LIBS[I_MAD_LIBS];	/* choose corresponding mad lib */
 
-console.log(RANDOM_MAD_LIB.template([1, 2, 3, 4, 5, 6])); /* test the template function */
+/* create the input array */
+const INPUT = [];
+
+/* ask for a phrase of each type in RANDOM_MAD_LIB.types */
+for (const TYPE of RANDOM_MAD_LIB.types) {
+	INPUT.push(prompt(`Please enter ${TYPE}.`));
+} /* for (const TYPE of RANDOM_MAD_LIB.types) */
+
+/* apply the template */
+const RESULT = RANDOM_MAD_LIB.template(INPUT);
+/* print the result */
+console.log(RESULT);
