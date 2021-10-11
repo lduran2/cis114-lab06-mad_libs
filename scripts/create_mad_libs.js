8f499540927548c41b33c44662b0cd0be6ffb5ce
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2021-10-11 12:47:29 -0400

    added support for multiple rounds

--- scripts/create_mad_libs.js
+++ scripts/create_mad_libs.js
@@ -3,12 +3,15 @@
  * Chooses a random mad lib and applies it to input from the user to
  * create a very short story.
  * By        : Leomar Duran <https://github.com/lduran2>
- * When      : 2021-10-04t23:21
+ * When      : 2021-10-11t12:46
  * Where     : Community College of Philadelphia
  * For       : CIS 114/JavaScript I
- * Version   : 1.2.0
+ * Version   : 2.0.0
  *
  * CHANGELOG :
+ *     v2.0.0 - 2021-10-11t12:46
+ *         added support for multiple rounds
+ *
  *     v1.2.0 - 2021-10-04t23:21
  *         added the 3 billy goats mad lib
  *
@@ -26,8 +29,12 @@
 /* all of the mad libs */
 const MAD_LIBS = [
 	{
-		'template': 'Once upon a time, there were ${parseInt(input[0], 10) + 2} little ${input[1]}. They protected a ${input[2]} that spans a river ${Math.floor((parseInt(input[3], 10) + 3)*3.28)} feet ${Math.floor((parseInt(input[3], 10) + 3)*39.37) % 12} inches across. One day, a troll wanted to cross the ${input[2]} to ${input[4]}. It was a strong troll, weighing in at ${parseInt(input[5], 10) + 150} lbs.',
-		'types': [ 'a number', 'an animal (plural)', 'a landmark', 'a length [in meters]', 'a fun activity', 'a weight [in lbs]' ]
+		rounds: [
+			{
+				template: 'Once upon a time, there were ${parseInt(input[0], 10) + 2} little ${input[1]}. They protected a ${input[2]} that spans a river ${Math.floor((parseInt(input[3], 10) + 3)*3.28)} feet ${Math.floor((parseInt(input[3], 10) + 3)*39.37) % 12} inches across. One day, a troll wanted to cross the ${input[2]} to ${input[4]}. It was a strong troll, weighing in at ${parseInt(input[5], 10) + 150} lbs.',
+				types: [ 'a number', 'an animal (plural)', 'a landmark', 'a length [in meters]', 'a fun activity', 'a weight [in lbs]' ]
+			}
+		]
 	}
 ];
 
@@ -38,14 +45,17 @@ const RANDOM_MAD_LIB = MAD_LIBS[I_MAD_LIBS];	/* choose corresponding mad lib */
 /* create the input array */
 const INPUT = [];
 
-/* ask for a phrase of each type in RANDOM_MAD_LIB.types */
-for (const TYPE of RANDOM_MAD_LIB.types) {
-	INPUT.push(prompt(`Please enter ${TYPE}.`));
-} /* for (const TYPE of RANDOM_MAD_LIB.types) */
+/* loop through each round of this mad lib */
+for (const ROUND of RANDOM_MAD_LIB.rounds) {
+	/* ask for a phrase of each type in ROUND.types */
+	for (const TYPE of ROUND.types) {
+		INPUT.push(prompt(`Please enter ${TYPE}.`));
+	} /* end for (const TYPE of ROUND.types) */
 
-/* apply the template */
-/* since template literals are static runtime constants,
- * they need a function wrapper to be compiled dynamically */
-const RESULT = new Function('input', 'return `' + RANDOM_MAD_LIB.template + '`;')(INPUT);
-/* print the result */
-console.log(RESULT);
+	/* apply the template */
+	/* since template literals are static runtime constants,
+	 * they need a function wrapper to be compiled dynamically */
+	const RESULT = new Function('input', 'return `' + ROUND.template + '`;')(INPUT);
+	/* print the result */
+	console.log(RESULT);
+} /* end for (const ROUND of RANDOM_MAD_LIB.rounds) */
